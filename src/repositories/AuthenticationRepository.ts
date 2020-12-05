5cf22e2f47015d0d3559b95611a7791b10bc84e1
Daniil <zelenko.danil@gmail.com>
2020-12-05 18:26:23 +0200

    Authentication repo

---
+++ src/repositories/AuthenticationRepository.ts
@@ -0,0 +1,31 @@
+import {AuthRepositoryI, UserData} from "../models/AuthenticationModel";
+import {Redis} from "ioredis";
+import {Connection, Pool} from "pg";
+
+export class AuthenticationRepository implements AuthRepositoryI{
+    redisConn: Redis;
+    pgConn: Pool;
+
+    constructor(redisConn: Redis, pgConn) {
+        this.redisConn = redisConn;
+        this.pgConn = pgConn;
+    }
+
+    async getUserByName(nickname: string) {
+        let result = await this.pgConn.query<UserData | undefined>(
+            "select * from users where nickname=$1",
+            [nickname]
+        );
+        return result.rows[0];
+    }
+
+    createSession(session_id: string, data: UserData) {
+        return this.redisConn.hmset(`user:${session_id}`, {...data});
+    }
+    async getUserBySession(session_id: string): Promise<UserData | undefined> {
+        return this.redisConn.hgetall<UserData | undefined>(session_id);
+    }
+    destroySession(session_id: string) {
+        this.redisConn.del(session_id);
+    }
+}
