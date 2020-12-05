d51cefe5fef6f1633b1586fa90b712e0f9deb11a
Daniil <zelenko.danil@gmail.com>
2020-12-05 16:38:47 +0200

    AuthenticationController init. login endpoint

---
+++ src/controllers/AuthenticationController.ts
@@ -0,0 +1,20 @@
+import {AuthenticationModel, UserData} from "../models/AuthenticationModel";
+import {post} from "../lib/httpMethodDecorators";
+import {Request, Response} from "../lib/ExtendContext";
+
+export class AuthenticationController {
+    authModel: AuthenticationModel;
+    constructor(model: AuthenticationModel) {
+        this.authModel = model;
+    }
+
+    @post("/login")
+    async login(req: Request, res: Response) {
+        let name = req.body.get("name");
+        let password = req.body.get("password");
+
+        let user: UserData = await this.authModel.verifyCredential(name, password);
+        let session_id: string = await this.authModel.createSession(user);
+        res.cookie("s_id", session_id);
+    }
+}
