6217db0d7bf84c7026455fede85624d1414fab99
Daniil <zelenko.danil@gmail.com>
2020-12-05 18:28:15 +0200

    clean, change client to pool

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,19 @@
 import * as redis from "promise-redis";
 import * as dotenv from "dotenv";
-import {Client} from "pg";
+import {Pool} from "pg";
 
 import {Bootstrap} from "./bootstrap";
 import * as http from "http";
-import {PostBody} from "./lib/PostBody";
 
 const app = http.createServer();
 dotenv.config();
 
 //create connection to data storage
 export const redisClient = redis().createClient();
-redisClient.on("error", (error: RedisError) => {
+redisClient.on("error", (error: any) => {
     console.error(error);
 });
-export const dbConnection = new Client({
+export const dbConnection = new Pool({
     user: process.env.DB_USER,
     host: process.env.DB_HOST,
     database: process.env.DB_BASE,
