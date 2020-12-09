6cf1298fc727a6cbc6841a1976cc1c90fc451e8e
Daniil <zelenko.danil@gmail.com>
2020-12-09 12:23:08 +0200

    thread repo

--- src/repositories/ThreadRepository.ts
+++ src/repositories/ThreadRepository.ts
@@ -1,6 +1,34 @@
 import {ThreadRepositoryI} from "../models/ThreadModel";
+import {Pool} from "pg";
+import {Redis} from "ioredis";
+import {DatabaseError} from "../lib/Error";
 
 export class ThreadRepository implements ThreadRepositoryI {
-    async getThreads() {}
-    async getPosts(thread_id: number) {}
+    pg: Pool;
+    redisConn: Redis;
+
+    constructor(redisConn: Redis, pgConn) {
+        this.redisConn = redisConn;
+        this.pg = pgConn;
+    }
+
+    async getThreads() {
+        try {
+            let result = await this.pg.query("select * from threads");
+            return result.rows[0];
+        } catch (e) {
+            throw new DatabaseError();
+        }
+    }
+    async getPosts(thread_id: number) {
+        try {
+            let result = await this.pg.query(
+                "select * from post where post.thread_id = $1",
+                [thread_id]
+            );
+            return result.rows[0];
+        } catch (e) {
+            throw new DatabaseError();
+        }
+    }
 }
