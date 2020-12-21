8f88571186631fcc60061a12e179a30ca3b66300
Daniil <zelenko.danil@gmail.com>
2020-12-21 12:48:10 +0200

    header

---
+++ frontend/src/Header.js
@@ -0,0 +1,14 @@
+import Link from "react-router-dom"
+
+export function Header() {
+    return (
+        <header>
+            <ul>
+                <li><Link to="/"/>Home</li>
+                <li><Link to="/"/>Home</li>
+                <li><Link to="/"/>Home</li>
+                <li><Link to="/"/>Home</li>
+            </ul>
+        </header>
+    )
+}
