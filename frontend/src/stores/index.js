2adaad7b746d0db8d5273a4c39cb3ef996bfb730
Daniil <zelenko.danil@gmail.com>
2020-12-21 12:51:45 +0200

    create stores

--- frontend/src/stores/index.js
+++ frontend/src/stores/index.js
@@ -1,5 +1,9 @@
-
+import {ThreadStore} from "./ThreadStore";
+import {TopicsStore} from "./TopicsStore";
+import {UserStore} from "./UserStore";
 
 export default {
-
+    threadStore: new ThreadStore(),
+    topicStore: new TopicsStore(),
+    userStore: new UserStore(),
 };
