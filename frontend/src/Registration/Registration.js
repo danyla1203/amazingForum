feeafe7c51ea3cdbfa7913d059b83613f7b9ede2
Daniil <zelenko.danil@gmail.com>
2020-12-22 10:25:03 +0200

    create new user

---
+++ frontend/src/Registration/Registration.js
@@ -0,0 +1,26 @@
+import React from "react";
+import {Redirect} from "react-router-dom";
+
+@inject("userStore")
+@observer
+export class Registration extends React.Component {
+    signIn() {
+        let form = document.getElementById("signin_form");
+        let formData = new FormData(form);
+        this.props.userStore.createUser(formData)
+    }
+
+    render() {
+        if (!this.props.userStore.user) {
+            return (
+                <form id="signin_form">
+                    <input type="text" name="name"/>
+                    <input type="password" name="password"/>
+                    <button type="button" onClick={this.signIn}>Sign in</button>
+                </form>
+            )
+        } else {
+            return <Redirect to="/" />
+        }
+    }
+}
--- frontend/src/stores/UserStore.js
+++ frontend/src/stores/UserStore.js
@@ -4,6 +4,16 @@ export class UserStore {
     @observable user = null;
     @observable errors = null;
 
+    createUser(userData) {
+        fetch(`${process.env.API_HOST}/register`, {method: "POST", body: userData})
+            .then((response) => {
+                return response.json()
+            })
+            .then((user) => {
+                this.user = user;
+            })
+    }
+
     login(inputedData) {
         fetch(`${process.env.API_HOST}/login`, {method: "POST", body: inputedData})
             .then((response) => {
@@ -11,9 +21,9 @@ export class UserStore {
             })
             .then((user) => {
                 if(user.status !== 200) {
-                    this.errors = user.statusText;
+                    this.errors = user;
                 } else {
-                    this.user = user.statusText;
+                    this.user = user;
                 }
             })
     }
