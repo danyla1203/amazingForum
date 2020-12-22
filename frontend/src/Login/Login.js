b634c8ca6c886a7677a90a1ab12e60fdda81a9c5
Daniil <zelenko.danil@gmail.com>
2020-12-22 20:27:32 +0200

    bind this for methods. Show errors

--- frontend/src/Login/Login.js
+++ frontend/src/Login/Login.js
@@ -5,6 +5,11 @@ import { observer, inject } from "mobx-react";
 @inject("userStore")
 @observer
 export default class Login extends React.Component {
+    constructor(props) {
+        super(props);
+        this.login = this.login.bind(this);
+    }
+
     login() {
         let form = document.getElementById("login_form");
         let formData = new FormData(form);
@@ -13,8 +18,10 @@ export default class Login extends React.Component {
 
     render() {
         if (!this.props.userStore.user) {
+            let errMessage = <h3>{this.props.userStore.errors}</h3>;
             return (
                 <form id="login_form">
+                    {errMessage}
                     <input type="text" name="name"/>
                     <input type="password" name="password"/>
                     <button type="button" onClick={this.login}>Sign in</button>
