b642f909f1304f04cf802fac51c161ad4cdc81aa
Daniil <zelenko.danil@gmail.com>
2020-12-23 12:18:27 +0200

    return threads with count of topics in it

--- src/Thread/ThreadRepository.ts
+++ src/Thread/ThreadRepository.ts
@@ -5,7 +5,15 @@ import {Repository} from "../lib/Repository";
 export class ThreadRepository extends Repository implements ThreadRepositoryI {
     async getThreads() {
         try {
-            let result = await this.pg.query("select * from threads");
+            let countTopicsInThread =
+                "select count(*), thread_id from topics\n" +
+                "group by topics.thread_id";
+            let threadsWithCount =
+                `Select threads.thread_id, name, count from threads
+                 join (${countTopicsInThread}) as t1 
+                 ON threads.thread_id = t1.thread_id`;
+
+            let result = await this.pg.query(threadsWithCount);
             return result.rows || [];
         } catch (e) {
             throw new DatabaseError(e.message);
