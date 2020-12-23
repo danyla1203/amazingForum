bfe0914e0afe652a805b79bf9d558b5089fb524e
Daniil <zelenko.danil@gmail.com>
2020-12-23 12:35:35 +0200

    update Home

--- frontend/src/User/Home.js
+++ frontend/src/User/Home.js
@@ -4,11 +4,16 @@ import { observer, inject } from "mobx-react";
 
 @inject("userStore")
 @observer
-export default class Home {
+export default class Home extends React.Component{
     render() {
-        if (this.props.userStore.user) {
+        let user = this.props.userStore.user;
+        if (user) {
             return (
-                <div>User page</div>
+                <div>
+                    <h3>{ user.name }</h3>
+                    <h4>{ user.email }</h4>
+                    <h4>{ user.country }</h4>
+                </div>
             )
         } else {
             return <Redirect to="/login" />
