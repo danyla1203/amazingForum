c409aeca60f4b970b16b746f64a1dacc6884c87d
Daniil <zelenko.danil@gmail.com>
2020-12-25 19:43:39 +0200

    update test

--- test/repository_method.test.ts
+++ test/repository_method.test.ts
@@ -4,9 +4,29 @@ import * as redis from "redis-mock"
 
 const repo = new Repository(redis.createClient(), new Pool());
 describe("test get values and column string", () => {
-    test("only strings", () => {
-        let obj = { name: "Name", age: 12, password: "1234", id: "1234123" };
-        let expected = ["name, age, password, id", "'Name', 12, '1234', '1234123'"];
+    test("test get values and column string", () => {
+        let obj = {
+            name: "Name",
+            age: 12,
+            password: "1234",
+            id: "1234123" };
+        let expected = [
+            "name, age, password, id",
+            "'Name', 12, '1234', '1234123'"
+        ];
         expect(repo.getFieldValuesString(obj)).toStrictEqual(expected);
-    })
+    });
+});
+
+describe("test getSetPair", () => {
+    test("test getSetPair", () => {
+        let obj = {
+            id: 123,
+            name: "TestName",
+            age: 13,
+            password: "t123"
+        };
+        let expected = "id = 123, name = 'TestName', age = 13, password = 't123'";
+        expect(repo.getSetPair(obj)).toStrictEqual(expected);
+    });
 });
