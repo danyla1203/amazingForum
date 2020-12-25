aeb7e9ecf1bb946930e65516469d9058883a012a
Daniil <zelenko.danil@gmail.com>
2020-12-25 19:27:41 +0200

    fix test

--- test/repository_method.test.ts
+++ test/repository_method.test.ts
@@ -6,7 +6,7 @@ const repo = new Repository(redis.createClient(), new Pool());
 describe("test get values and column string", () => {
     test("only strings", () => {
         let obj = { name: "Name", age: 12, password: "1234", id: "1234123" };
-        let expected = ["name, age, password, id", "'Name', 12, '1234', 1234123"];
-        expect(repo.getFieldValuesString(obj)).toBe(expected);
+        let expected = ["name, age, password, id", "'Name', 12, '1234', '1234123'"];
+        expect(repo.getFieldValuesString(obj)).toStrictEqual(expected);
     })
 });
