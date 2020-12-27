9ccdfb553469ce9552bc46237665115b7128eece
Daniil <zelenko.danil@gmail.com>
2020-12-27 21:01:58 +0200

    load comments

--- frontend/src/stores/CurrentTopicStore.js
+++ frontend/src/stores/CurrentTopicStore.js
@@ -2,6 +2,7 @@ import {observable} from "mobx/lib/mobx";
 
 export class CurrentTopicStore {
     @observable selected_topic = null;
+    @observable comments = null;
 
     loadSelectedTopic(topic_id) {
         fetch(`${process.env.API_HOST}/topic/${topic_id}`)
@@ -10,6 +11,16 @@ export class CurrentTopicStore {
             })
             .then((topic) => {
                 this.selected_topic = topic.payload;
+            })
+    }
+
+    loadComments(topic_id) {
+        fetch(`${process.env.API_HOST}/topic/${topic_id}/messages`)
+            .then((response) => {
+                return response.json();
+            })
+            .then((comments) => {
+                this.comments = comments.payload;
             })
     }
 }
