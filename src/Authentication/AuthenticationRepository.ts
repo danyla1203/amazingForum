cae76e992f11c24663e4fe0c11ff78a8a14b7e2d
Daniil <zelenko.danil@gmail.com>
2020-12-29 10:14:30 +0200

    handle errors

--- src/Authentication/AuthenticationRepository.ts
+++ src/Authentication/AuthenticationRepository.ts
@@ -1,23 +1,38 @@
 import {AuthRepositoryI} from "./AuthenticationModel";
 import {UserData} from "./types";
 import {Repository} from "../lib/Repository";
+import {DatabaseError} from "../lib/Error";
 
 export class AuthenticationRepository extends Repository implements AuthRepositoryI{
     async getUserByName(nickname: string) {
-        let result = await this.pg.query<UserData>(
-            "select * from users where nickname=$1",
-            [nickname]
-        );
-        return result.rows[0];
+        try {
+            let sql = "select * from users where nickname=$1";
+            let result = await this.pg.query<UserData>(sql, [nickname]);
+            return result.rows[0];
+        } catch (e) {
+            throw new DatabaseError(e);
+        }
     }
 
     createSession(session_id: string, data: UserData) {
-        return this.redisConn.hmset(`user:${session_id}`, {...data});
+        try {
+            return this.redisConn.hmset(`user:${session_id}`, {...data});
+        } catch (e) {
+            throw new DatabaseError(e);
+        }
     }
     async getUserBySession(session_id: string): Promise<UserData | undefined> {
-        return this.redisConn.hgetall<UserData | undefined>(session_id);
+        try {
+            return this.redisConn.hgetall<UserData | undefined>(session_id);
+        } catch (e) {
+            throw new DatabaseError(e);
+        }
     }
     destroySession(session_id: string) {
-        this.redisConn.del(session_id);
+        try {
+            this.redisConn.del(session_id);
+        } catch (e) {
+            throw new DatabaseError(e);
+        }
     }
 }
