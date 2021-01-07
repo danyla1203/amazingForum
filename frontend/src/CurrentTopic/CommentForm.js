4f39d4a7eeaf46d1e342c2cdaf082688ff6c1ca0
Daniil <zelenko.danil@gmail.com>
2021-01-07 14:39:15 +0200

    open/close comment form

--- frontend/src/CurrentTopic/CommentForm.js
+++ frontend/src/CurrentTopic/CommentForm.js
@@ -1,15 +1,28 @@
-import React from "react"
+import React, { useState } from "react"
 
 export function CommentForm(props) {
+    const [isOpen, toogle] = useState(false);
+
     const submitComment = () => {
         let form = document.getElementById("comment_form");
         let formData = new FormData(form);
         props.submitComment(formData);
     };
-    return (
-        <form id="comment_form">
-            <textarea name="text" cols="30" rows="10"></textarea>
-            <button type="button" onClick={submitComment}>Comment</button>
-        </form>
-    )
+    const toggleComentForm = () => {
+       toogle(!isOpen);
+    };
+
+    if (isOpen) {
+        return (
+            <form id="comment_form">
+                <textarea name="text" cols="30" rows="10"></textarea>
+                <button type="button" onClick={submitComment}>Comment</button>
+                <button onClick={toggleComentForm}>Close</button>
+            </form>
+        )
+    } else {
+        return (
+            <button onClick={toggleComentForm}>Write comment</button>
+        )
+    }
 }
