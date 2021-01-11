4306c342e00eb5e7ae3d0993d43b7ef2d2f6e7fc
Daniil <zelenko.danil@gmail.com>
2021-01-11 12:47:32 +0200

    insert comment in store

--- frontend/src/stores/CurrentTopicStore.js
+++ frontend/src/stores/CurrentTopicStore.js
@@ -1,5 +1,4 @@
 import {observable} from "mobx/lib/mobx";
-
 export class CurrentTopicStore {
     @observable selected_topic = null;
     @observable comments = [];
@@ -16,16 +15,17 @@ export class CurrentTopicStore {
             })
     }
 
-    addComment(topic_id, comment) {
-        this.comments.push(comment);
-        fetch(`${process.env.API_HOST}/topic/${topic_id}/add-comment`, { method: "POST", body: comment })
+    addComment(topic_id, commentFormData) {
+        let commentForInsert = {
+            id: this.comments[this.comments.length - 1].id,
+            text: commentFormData.get("text"),
+            date: "2003.12.03"
+        };
+        this.comments.push(commentForInsert);
+        
+        fetch(`${process.env.API_HOST}/topic/${topic_id}/add-comment`, { method: "POST", body: commentFormData })
             .then((response) => {
                 return response.json();
-            })
-            .then((topic) => {
-                if (topic.statusCode == 200) {
-                    this.selected_topic = topic.payload;
-                }
             })
     }
 
