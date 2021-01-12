d631e07a9903129330552094527c4fc3ea4a9701
Daniil <zelenko.danil@gmail.com>
2021-01-12 13:16:37 +0200

    update topics count in cache

--- src/Post/PostRepository.ts
+++ src/Post/PostRepository.ts
@@ -6,7 +6,6 @@ import {Comment, IncomingComment, IncomingTopic, Topic} from "./types";
 export class PostRepository extends Repository implements PostRepoI{
     async getCommentsForTopic(post_id: number): Promise<Comment[]> {
         try {
-            //TODO: repair sql
             let sql =
                 `select comments.id, comments.text, users.nickname as author_name, users.avatar_path as user_avatar, comments.date from comments
                  join topics
@@ -14,7 +13,6 @@ export class PostRepository extends Repository implements PostRepoI{
                  join users
                  on users.id = comments.author_id
                  where comments.topic_id = ${post_id}`;
-            console.log(sql);
             let comments = await this.pg.query(sql);
             return comments.rows;
         } catch (e) {
@@ -33,6 +31,9 @@ export class PostRepository extends Repository implements PostRepoI{
     }
     async createTopic(topic: IncomingTopic): Promise<Topic> {
         try {
+            //update topics count in thread
+            this.redisConn.hash.incby(`threads:${topic.thread_id}`, "count", 1);
+
             let [field, values] = this.getFieldValuesString(topic);
             let sql = `insert into topics(${field}) values(${values}) returning *`;
             let result = await this.pg.query<Topic>(sql);
