ac64d303411b4ed97b31f14908b2af3749ca7ffb
Daniil <zelenko.danil@gmail.com>
2021-01-12 12:45:49 +0200

    create new topic

--- frontend/src/Topics/CreateTopic.js
+++ frontend/src/Topics/CreateTopic.js
@@ -1,7 +1,40 @@
 import React from "react";
+import { inject } from "mobx-react";
 
-export function CreateTopic() {
-    return (
-        <div id="creater"></div>
-    )
+@inject("topicStore", "threadStore")
+export class  CreateTopic extends React.Component {
+    constructor() {
+        super();
+        this.createArticle = this.createArticle.bind(this);
+    }
+
+    createArticle() {
+        let form = document.getElementById("createTopic_form");
+        let formData = new FormData(form);
+        this.props.topicStore.createTopic(formData);
+    }
+
+    renderSelectTag(threads) {
+        let optionsList = threads.map((thread) => {
+            let id = thread.thread_id;
+            return <option key={id} value={id}>{ thread.name }</option>;
+        });
+        return <select name="thread_id">{ optionsList }</select>
+    }
+    render() {
+        let selectTag = this.renderSelectTag(this.props.threadStore.threads);
+        return (
+            <div id="creater">
+                <form id="createTopic_form">
+                    <input type="text" name="title"/>
+                    <textarea name="text" cols="30" rows="10"></textarea>
+                    { selectTag }
+                    <button onClick={this.createArticle} type="button">Create</button>
+                </form>
+                <div id="article_prev">
+                
+                </div>
+            </div>
+        )
+    }
 }
--- frontend/src/stores/TopicsStore.js
+++ frontend/src/stores/TopicsStore.js
@@ -12,4 +12,21 @@ export class TopicsStore {
                 this.topics = topics.payload;
             })
     }
+    
+    createTopic(formDataTopic) {
+        let lastTopic = this.topics[this.topics.length - 1];
+        const topic = {
+            id: lastTopic ? lastTopic.id + 1 : 0,
+            title: formDataTopic.get("title"),
+        };
+        this.topics.push(topic);
+
+        fetch(`${process.env.API_HOST}/topic/create`, {method: "POST", body: formDataTopic})
+            .then((response) => {
+                return response.json();
+            })
+            .then((topics) => {
+
+            })
+    }
 }
